9dbf51bb27c2654a0db47c1baaac783177c90ab6
chauhanshallu <shalluchauhan1307@gmail.com>
2024-12-15 00:51:33 +0100

    added stopwords

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import string
 from nltk.corpus import stopwords
 import nltk
 nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
